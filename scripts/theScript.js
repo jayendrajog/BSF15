d2b302513d0633d23126c2e0a8f816a97a63b0ed
Jayendra <jay08jog@gmail.com>
2015-09-14 12:48:46 -0700

    finished about section:

--- scripts/theScript.js
+++ scripts/theScript.js
@@ -2,16 +2,27 @@ window.onload = function(){
 	window.addEventListener('scroll', function(e)
 	{
         var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-            shrinkOn = 300,
+            shrinkOn = 350,
             navbar = document.querySelector(".navbar"),
             header = document.querySelector(".navbar-left"),
             header2 = document.querySelector(".navbar-right");
+        if(distanceY > shrinkOn - 100 && distanceY <= shrinkOn)
+        {
+        	$('.navbar').css('background-color', 'rgba(0, 0, 0, 1)');
+        }
+        else if(distanceY < shrinkOn)
+        {
+        	$('.navbar').css('background-color', 'rgba(0, 0, 0, 0)');
+        }
         if (distanceY > shrinkOn) {
+        	$('.navbar').css('background-color', 'rgba(0, 0, 0, 1)');
         	classie.add(navbar,"smaller");
             classie.add(header,"smaller");
             classie.add(header2, "smaller");
-        } else {
+        } 
+        else {
             if (classie.has(header,"smaller")) {
+            	$('.navbar').css('background-color', 'rgba(0, 0, 0, 1)');
             	classie.remove(navbar,"smaller");
                 classie.remove(header,"smaller");
                 classie.remove(header2,"smaller");
@@ -19,14 +30,3 @@ window.onload = function(){
         }
     });
 }
-
-var theoffset = $(".navbar").offset().top;
-
-$(document).scroll(function(){
-    if($(this).scrollTop() > theoffset + 200)
-    {   
-       $('.navbar').css({"background":"black"});
-    } else {
-       $('.navbar').css({"background":"transparent"});
-    }
-});
