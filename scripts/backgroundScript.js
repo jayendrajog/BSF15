20ca578124f2625044c6761846da66615f37aa26
Jayendra <jay08jog@gmail.com>
2015-09-20 22:28:13 -0700

    more updates

--- scripts/backgroundScript.js
+++ scripts/backgroundScript.js
@@ -1,4 +1,5 @@
- $(function() {
+ $(function() 
+ {
 
   var $body = $(document.body),
    $window = $(window);
@@ -51,28 +52,3 @@
     initBackground();
   }
 });
-
-//  // 1) Create a Renderer for the context you would like to render to.
-// //    You can use either the WebGLRenderer, CanvasRenderer or SVGRenderer.
-// var renderer = new FSS.CanvasRenderer();
-
-// // 2) Add the Renderer's element to the DOM:
-// var container = document.getElementById('container');
-// container.appendChild(renderer.element);
-
-// // 3) Create a Scene:
-// var scene = new FSS.Scene();
-
-// // 4) Create some Geometry & a Material, pass them to a Mesh constructor, and add the Mesh to the Scene:
-// var geometry = new FSS.Plane(200, 100, 4, 2);
-// var material = new FSS.Material('#444444', '#FFFFFF');
-// var mesh = new FSS.Mesh(geometry, material);
-// scene.add(mesh);
-
-// // 5) Create and add a Light to the Scene:
-// var light = new FSS.Light('#FF0000', '#0000FF');
-// scene.add(light);
-
-// // 6) Finally, render the Scene:
-// renderer.render(scene);
-
