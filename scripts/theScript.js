c68151572d471e53b671450c16de6905dfa1f3e1
Jayendra <jay08jog@gmail.com>
2015-09-23 19:52:43 -0700

    more updates

--- scripts/theScript.js
+++ scripts/theScript.js
@@ -1,35 +1,34 @@
-
-
 window.onload = function(){
 	var NAV_SCROLL_MAX = 450;
 	window.addEventListener('scroll', function(e)
 	{
+        var width = window.outerWidth;
         var distanceY = window.pageYOffset || document.documentElement.scrollTop,
             shrinkOn = 600,
             navbar = document.querySelector(".navbar"),
             header = document.querySelector(".navbar-left"),
-            header2 = document.querySelector(".navbar-right");
-        var shading = distanceY/NAV_SCROLL_MAX;
-        if(distanceY > NAV_SCROLL_MAX && distanceY <= shrinkOn)
+            header2 = document.querySelector(".navbar-right");  
+        if((distanceY > NAV_SCROLL_MAX && distanceY <= shrinkOn) || width < 993)
         {
         	$('.navbar').css('background-color', 'rgba(0, 0, 0, 1)');
         }
         else if(distanceY < NAV_SCROLL_MAX)
         {
+            var shading = distanceY/NAV_SCROLL_MAX;
         	$('.navbar').css('background-color', 'rgba(0, 0, 0,' + shading +')');
         }
         if (distanceY > shrinkOn) {
         	$('.navbar').css('background-color', 'rgba(0, 0, 0, 1)');
-        	classie.add(navbar,"smaller");
-            classie.add(header,"smaller");
-            classie.add(header2, "smaller");
+        	// classie.add(navbar,"smaller");
+         //    classie.add(header,"smaller");
+         //    classie.add(header2, "smaller");
         } 
         else {
             if (classie.has(header,"smaller")) {
             	$('.navbar').css('background-color', 'rgba(0, 0, 0, 1)');
-            	classie.remove(navbar,"smaller");
-                classie.remove(header,"smaller");
-                classie.remove(header2,"smaller");
+            	// classie.remove(navbar,"smaller");
+             //    classie.remove(header,"smaller");
+             //    classie.remove(header2,"smaller");
             }
         }
     });
@@ -43,4 +42,3 @@ $('a').click(function() {
 
     return false;
 });
-
